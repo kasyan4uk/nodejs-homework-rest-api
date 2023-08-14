3b639e850e4765993e39b5e2bd7348bf3cd3dacd
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-08-14 23:35:53 +0300

    fix verify

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,7 +51,7 @@ const verifyEmail = async (req, res) => {
     if (!user) {
         throw HttpError(404, "User not found")
     }
-    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: "" });
+    await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: null });
 
     res.json({
         message: 'Verification successful',
